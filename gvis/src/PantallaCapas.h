8d173126ea0a7dd6134af9dd2509f7ba022259d1
Gabriel <gabriel-tandil@users.noreply.github.com>
2010-04-05 03:42:10 +0000

    formateo

--- gvis/src/PantallaCapas.h
+++ gvis/src/PantallaCapas.h
@@ -1,20 +1,22 @@
-//    This file is part of gvis.
-//
-//    Copyright (C) 2008, 2009, 2010
-//       Gabriel Alvarez
-//    
-//    gvis is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation, either version 3 of the License, or
-//    (at your option) any later version.
-//
-//    Foobar is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
-//
-//    You should have received a copy of the GNU General Public License
-//    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+/*
+ * This file is part of gvis.
+ *
+ * Copyright (C) 2008, 2009, 2010
+ *    Gabriel Alvarez
+ *
+ * gvis is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * gvis is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with gvis.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef PANTALLACAPAS_H_
 #define PANTALLACAPAS_H_
@@ -45,7 +47,7 @@ public:
   void add_columns();
   void liststore_add_item(const Capa& foo);
 
-  struct ModelColumns : public Gtk::TreeModelColumnRecord
+  struct ModelColumns : public Gtk::TreeModel::ColumnRecord
     {
     Gtk::TreeModelColumn<bool> cargada;
     Gtk::TreeModelColumn<Glib::ustring> nombre;
--- gvis/src/util/Capa.h
+++ gvis/src/util/Capa.h
@@ -1,20 +1,22 @@
-//    This file is part of libvis.
-//
-//    Copyright (C) 2008, 2009, 2010
-//       Gabriel Alvarez
-//    
-//    libvis is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation, either version 3 of the License, or
-//    (at your option) any later version.
-//
-//    Foobar is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
-//
-//    You should have received a copy of the GNU General Public License
-//    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+/*
+ * This file is part of gvis.
+ *
+ * Copyright (C) 2008, 2009, 2010
+ *    Gabriel Alvarez
+ *
+ * gvis is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * gvis is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with gvis.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef CAPA_H_
 #define CAPA_H_
